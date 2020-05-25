0ec075d4d6f18259838864eb91004ad06c683ce6
Kafil Nasdami <kafil.nasdami@gmail.com>
2020-05-25 22:22:13 +0000

    Fix city list

--- src/context/actions.js
+++ src/context/actions.js
@@ -3,7 +3,7 @@ import { INITIAL_INIT } from "./types";
 export const initState = (dispatch) => {
   const id = localStorage.getItem("id") || 80;
   const theme = localStorage.getItem("theme") || "light";
-  const lang = localStorage.getItem("lang") || "ar-ar";
+  const lang = localStorage.getItem("lang") || "ar-ma";
   const periodicity = localStorage.getItem("periodicity") || "daily";
   dispatch({ type: INITIAL_INIT, payload: { id, theme, lang, periodicity } });
 };

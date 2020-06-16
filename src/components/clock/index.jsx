841432773e997b34fd3eac57eddfd474bb2c9860
Kafil Nasdami <kafil.nasdami@gmail.com>
2020-06-16 22:51:03 +0100

    Using refs instead of multiple rerenders

--- src/components/clock/index.jsx
+++ src/components/clock/index.jsx
@@ -1,12 +1,28 @@
-import { FormattedDate } from "react-intl";
-import { H2, H3, Wrapper } from "./styles";
+import moment from 'moment';
+import { useRef } from 'react';
+import { FormattedDate, useIntl } from 'react-intl';
+import useInterval from '../../hooks/useInterval';
+import { H2, H3, Wrapper } from './styles';
+const Clock = ({ displayTime = false }) => {
+  const timeRef = useRef(moment());
+  const intl = useIntl();
+  const timeFormat = () =>
+    intl.formatTime(moment(), {
+      hour: 'numeric',
+      minute: 'numeric',
+      second: 'numeric',
+      timeZone: 'Africa/Casablanca',
+    });
 
-const Clock = ({ displayTime = false, day, time }) => {
+  const setRef = (ref) => (ref.current.textContent = timeFormat());
+
+  useInterval(() => setRef(timeRef), 1000);
+
   return (
     <Wrapper>
       <H2>
         <FormattedDate
-          value={day}
+          value={moment()}
           year="numeric"
           month="long"
           timeZone="Africa/Casablanca"
@@ -15,14 +31,8 @@ const Clock = ({ displayTime = false, day, time }) => {
         ></FormattedDate>
       </H2>
       {displayTime && (
-        <H3 suppressHydrationWarning>
-          <FormattedDate
-            value={time}
-            hour="numeric"
-            minute="numeric"
-            second="numeric"
-            timeZone="Africa/Casablanca"
-          ></FormattedDate>
+        <H3 suppressHydrationWarning ref={timeRef}>
+          {timeFormat()}
         </H3>
       )}
     </Wrapper>
--- src/components/daily/index.jsx
+++ src/components/daily/index.jsx
@@ -1,46 +1,50 @@
-import moment from "moment";
-import { useRouter } from "next/router";
-import React, { useContext, useEffect, useState } from "react";
-import Spinner from "../../common/spinner";
-import { AppContext } from "../../context/AppContext";
-import useInterval from "../../hooks/useInterval";
-import { DEFAULT_TIME_FORMAT } from "../../settings";
-import { parseTime } from "../../utils/dates";
-import Clock from "../clock";
-import SelectList from "../select-list";
-import { Difference, Li, Name, Time, Ul } from "./styles";
+import moment from 'moment';
+import { useRouter } from 'next/router';
+import React, { useContext, useRef, useState } from 'react';
+import Spinner from '../../common/spinner';
+import { AppContext } from '../../context/AppContext';
+import { DAILY } from '../../context/types';
+import useInterval from '../../hooks/useInterval';
+import { DEFAULT_TIME_FORMAT } from '../../settings';
+import { parseTime } from '../../utils/dates';
+import Clock from '../clock';
+import SelectList from '../select-list';
+import { Difference, Li, Name, Time, Ul } from './styles';
 
-const NAMES = require("../../../public/data/prayerNames.json");
+const NAMES = require('../../../public/data/prayerNames.json');
 
 const Daily = ({ prayers }) => {
-  const { lang, slug, cities, periodicity } = useContext(AppContext);
+  const { lang, slug, cities } = useContext(AppContext);
+  const diffRef = useRef();
+  const nextRef = useRef();
+  const timeRef = useRef();
+  const [next, setNext] = useState(Object.keys(NAMES)[0]);
 
   const router = useRouter();
   let prayer = (prayers || [])[0];
 
-  const [state, setState] = useState({});
-  const [time, setTime] = useState(moment());
-
   const thick = () => {
-    setTime(moment());
+    if (prayer) {
+      timeRef.current = moment();
+      const nextOnes = Object.keys(prayer).filter((t) =>
+        timeRef.current.utc().isBefore(moment.utc(prayer[t]))
+      );
+      const localNext =
+        nextOnes.length === 0 ? Object.keys(NAMES)[0] : nextOnes[0];
+      const diff = moment
+        .utc(moment.utc(prayer[localNext]).diff(timeRef.current.utc()))
+        .format(DEFAULT_TIME_FORMAT);
+      nextRef.current = localNext;
+      if (nextRef.current !== next) {
+        setNext(nextRef.current);
+      }
+      if (diffRef.current) {
+        diffRef.current.textContent = diff;
+      }
+    }
   };
 
-  useEffect(() => {
-    if (prayer) {
-      const nextOnes = Object.keys(prayer).filter((t) =>
-        time.utc().isBefore(moment.utc(prayer[t]))
-      );
-      const next = nextOnes.length === 0 ? Object.keys(NAMES)[0] : nextOnes[0];
-      const diff = moment
-        .utc(moment.utc(prayer[next]).diff(time.utc()))
-        .format(DEFAULT_TIME_FORMAT);
-      setState({ next, diff });
-    }
-  }, [prayer, time]);
-
   useInterval(thick, 1000);
-
-  const { next, diff } = state;
 
   return prayer ? (
     <>
@@ -51,19 +55,19 @@ const Daily = ({ prayers }) => {
             slug={slug}
             lang={lang}
             onChange={({ value }) => {
-              const redirect = `/${periodicity}/${value}`;
+              const redirect = `/${DAILY}/${value}`;
               router.push(`/[periodicity]/[slug]`, redirect);
             }}
           />
         </div>
       )}
-      <Clock displayTime time={time} day={prayer.day} />
+      <Clock displayTime />
       <Ul>
         {Object.keys(NAMES).map((name) => {
           return (
-            <Li key={name} lang={lang} className={name === next ? "next" : ""}>
-              <Name>{NAMES[name][lang === "ar-ma" ? "ar-ma" : "fr-fr"]}</Name>
-              {name === next && <Difference>{diff}</Difference>}
+            <Li key={name} lang={lang} className={name === next ? 'next' : ''}>
+              <Name>{NAMES[name][lang === 'ar-ma' ? 'ar-ma' : 'fr-fr']}</Name>
+              {name === next && <Difference ref={diffRef}></Difference>}
               <Time>{parseTime(prayer[name])}</Time>
             </Li>
           );

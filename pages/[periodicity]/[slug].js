b20c41426792871b4facea62e1f7e203f06e4e74
Kafil Nasdami <kafil.nasdami@gmail.com>
2020-06-18 00:55:54 +0100

    Incremental build

--- pages/[periodicity]/[slug].js
+++ pages/[periodicity]/[slug].js
@@ -1,27 +1,26 @@
-import { useRouter } from "next/router";
-import { useContext, useEffect } from "react";
-import cities from "../../public/data/cities.json";
-import Daily from "../../src/components/daily";
-import Monthly from "../../src/components/monthly";
-import { initState } from "../../src/context/actions";
-import { AppContext } from "../../src/context/AppContext";
-import { DAILY, MONTHLY, PERIODICITY, SLUG } from "../../src/context/types";
-import { getPrayers } from "../../src/utils/dataService";
+import { useRouter } from 'next/router';
+import { useContext, useEffect } from 'react';
+import cities from '../../public/data/cities.json';
+import Daily from '../../src/components/daily';
+import Monthly from '../../src/components/monthly';
+import { initState } from '../../src/context/actions';
+import { AppContext } from '../../src/context/AppContext';
+import { DAILY, MONTHLY, PERIODICITY, SLUG } from '../../src/context/types';
+import { getPrayers } from '../../src/utils/dataService';
 
 const PeriodicitySwitch = ({ prayers }) => {
   const router = useRouter();
 
   let { periodicity, slug } = router.query;
-  const {dispatch} = useContext(AppContext)
+  const { dispatch } = useContext(AppContext);
 
-  
   useEffect(() => {
-    localStorage.setItem(PERIODICITY, periodicity)
-    localStorage.setItem(SLUG, slug)
+    localStorage.setItem(PERIODICITY, periodicity);
+    localStorage.setItem(SLUG, slug);
     initState(dispatch);
     const redirect = `/${periodicity}/${slug}`;
     router.push(`/[periodicity]/[slug]`, redirect);
-  }, [periodicity,slug]);
+  }, [periodicity, slug]);
 
   return periodicity === DAILY ? (
     <Daily prayers={prayers} />
@@ -60,6 +59,7 @@ export async function getStaticProps({ params }) {
     props: {
       prayers,
     },
+    unstable_revalidate: 1,
   };
 }
 

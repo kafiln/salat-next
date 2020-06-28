4254fa9c79cc9cdeecc9dd05424e32c1a2b90aeb
Kafil Nasdami <kafil.nasdami@gmail.com>
2020-06-29 00:23:19 +0100

    Simplify the theme toggle

--- src/common/theme.jsx
+++ src/common/theme.jsx
@@ -1,24 +1,24 @@
-import React, { useContext } from "react";
-import MoonSVG from "../../public/moon.svg";
-import SunSVG from "../../public/sun.svg";
-import { AppContext } from "../context/AppContext";
-import { CHANGE_THEME, DARK, LIGHT } from "../context/types";
+import React, { useContext } from 'react';
+import { AppContext } from '../context/AppContext';
+import { CHANGE_THEME, DARK } from '../context/types';
+import Emoji from './emoji';
 
-function Theme() {
+const Theme = () => {
   const { dispatch, theme } = useContext(AppContext);
-  const themes = [DARK, LIGHT];
-  const availableTheme = themes.filter((t) => t !== theme)[0];
 
   const toggleTheme = () => dispatch({ type: CHANGE_THEME });
   return (
     <div
-      className="bg-gray-200 cursor-pointer border-0 mx-2 py-1 px-3 focus:outline-none hover:bg-gray-300 rounded text-base mt-4 md:mt-0"
+      className="bg-gray-200 rounded-full cursor-pointer border-0 mx-2 py-1 px-3 focus:outline-none hover:bg-gray-300  text-base mt-4 md:mt-0"
       onClick={toggleTheme}
     >
-      {theme === DARK ? <SunSVG /> : <MoonSVG />}
-      {availableTheme.toLowerCase()}
+      {theme === DARK ? (
+        <Emoji name="sun" icon="🌞" />
+      ) : (
+        <Emoji name="moon" icon="🌙" />
+      )}
     </div>
   );
-}
+};
 
 export default Theme;

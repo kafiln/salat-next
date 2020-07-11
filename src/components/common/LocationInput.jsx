ae19fa6b6df2413a4d9deab1ba86d4aaa526cf7b
Kafil Nasdami <kafil.nasdami@gmail.com>
2020-07-11 13:47:28 +0100

    Refactoring the select list 🎨

--- src/components/common/LocationInput.jsx
+++ src/components/common/LocationInput.jsx
@@ -1,24 +1,48 @@
 import { useRouter } from 'next/router';
-import React, { useContext } from 'react';
+import React, { useContext, useRef } from 'react';
 import { FaMapMarkerAlt } from 'react-icons/fa';
+import Select from 'react-select';
 import cities from '../../../public/data/cities.json';
 import { AppContext } from '../../context/AppContext';
-import SelectList from '../SelectList';
+
+const orderBy = field => (a, b) =>
+  a[field] > b[field] ? 1 : b[field] > a[field] ? -1 : 0;
 
 const LocationInput = ({}) => {
   const { slug, lang, periodicity, isRTL } = useContext(AppContext);
+  const options = cities
+    ? cities
+        .map(e => ({
+          value: e.slug,
+          label: e.names[isRTL ? 'ar-ma' : 'fr-fr']
+        }))
+        .sort(orderBy('label'))
+    : [];
+
+  const value = options.find(e => e.value == slug);
   const router = useRouter();
+  const selectRef = useRef();
+  const handleClick = () => {
+    selectRef.current.focus();
+    console.log(selectRef);
+  };
   return (
     <div
       className={`flex items-center justify-center max-w-md mx-auto ${isRTL &&
         'flex-row-reverse'} `}
     >
-      <FaMapMarkerAlt size="1.5em" />
+      <FaMapMarkerAlt
+        size="1.5em"
+        className="cursor-pointer"
+        onClick={handleClick}
+      />
       <div className="flex-1 mx-2">
-        <SelectList
-          cities={cities}
-          slug={slug}
-          lang={lang}
+        <Select
+          instanceId={slug}
+          // styles={customStyles}
+          options={options}
+          value={value}
+          ref={selectRef}
           isRtl={isRTL}
           onChange={({ value }) => {
             const redirect = `/${periodicity}/${value}`;

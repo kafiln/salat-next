1d93a060223a63b0b00aed4c31cb7034e5faf06a
Kafil Nasdami <kafil.nasdami@gmail.com>
2021-05-16 13:24:36 +0100

    Update timezone

--- src/settings.js
+++ src/settings.js
@@ -2,7 +2,7 @@ export const DEFAULT_TIME_FORMAT = 'HH:mm:ss';
 export const DEFAULT_DATE_FORMAT = 'dddd LL';
 
 //TODO: Use .env
-export const TIME_OFFSET = 0;
+export const TIME_OFFSET = 1;
 export const HIJRI_YEAR = 1442;
 export const TIMEZONE = 'Africa/Casablanca';
 export const APPLICATION_NAME = 'Salati';

b05e46d70f8c0bcbb034ff642a5856c60740305a
Kafil <kafil.nasdami@gmail.com>
2021-08-10 10:26:11 +0100

    New hijri hear: 1443

--- src/settings.js
+++ src/settings.js
@@ -3,7 +3,7 @@ export const DEFAULT_DATE_FORMAT = 'dddd LL';
 
 //TODO: Use .env
 export const TIME_OFFSET = 1;
-export const HIJRI_YEAR = 1442;
+export const HIJRI_YEAR = 1443;
 export const TIMEZONE = 'Africa/Casablanca';
 export const APPLICATION_NAME = 'Salati';
 export const APPLICATION_SOURCE = 'https://github.com/kafiil/salat-next';
